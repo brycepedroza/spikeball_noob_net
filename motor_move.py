d44a4d79a821697510535880ac5adc0a1ae56f47
Bryce Pedroza <bapedroz@asu.edu>
2020-04-25 21:45:49 -0700

    updated moving forward and backwards

--- motor_move.py
+++ motor_move.py
@@ -1,11 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-in1 = 24
-in2 = 23
-en = 25
-temp1=1
-
 # Initialize the motors
 motor1 = {
     "in_1": 24,
@@ -23,77 +18,77 @@ motor3 = {
     "en": 21
 }
 
-# Configure the GPIO Pins
+motors = [motor1, motor2, motor3]
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
+for motor in motors:
+    # Configure the GPIO Pins
+    GPIO.setmode(motor['in_1', GPIO.OUT])
+    GPIO.setmode(motor['in_2', GPIO.OUT])
+    GPIO.setmode(motor['en', GPIO.OUT])
+
+# GPIO.setup(in1,GPIO.OUT)
+# GPIO.setup(in2,GPIO.OUT)
+# GPIO.setup(en,GPIO.OUT)
+# GPIO.output(in1,GPIO.LOW)
+# GPIO.output(in2,GPIO.LOW)
 
 # Pulse Width Modulation
-p=GPIO.PWM(en,1000)
-p.start(25)
+p = GPIO.PWM(en, 1000)
+p.start(66)
+
 print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("wasd movement with q to stop and e to exit .....")
 print("\n")
 
 while(1):
 
-    x=raw_input()
+    ctrl = raw_input()
 
-    if x=='r':
-        print("run")
-        if(temp1==1):
-         GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in2,GPIO.LOW)
-         print("forward")
-         x='z'
-        else:
-         GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
-         print("backward")
-         x='z'
-
-
-    elif x=='s':
+    # wasd movement, q to stop.
+    if ctrl == 'q':
         print("stop")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        x='z'
+        GPIO.output(motor1['in_1'], GPIO.LOW)
+        GPIO.output(motor1['in_2'], GPIO.LOW)
+        GPIO.output(motor2['in_1'], GPIO.LOW)
+        GPIO.output(motor2['in_2'], GPIO.LOW)
+        GPIO.output(motor3['in_1'], GPIO.LOW)
+        GPIO.output(motor3['in_2'], GPIO.LOW)
 
-    elif x=='f':
+    if ctrl == 'w':
         print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        temp1=1
-        x='z'
+        GPIO.output(motor1['in_1'], GPIO.LOW)
+        GPIO.output(motor1['in_2'], GPIO.LOW)
+        GPIO.output(motor2['in_1'], GPIO.HIGH)
+        GPIO.output(motor2['in_2'], GPIO.LOW)
+        GPIO.output(motor3['in_1'], GPIO.HIGH)
+        GPIO.output(motor3['in_2'], GPIO.LOW)
 
-    elif x=='b':
-        print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        temp1=0
-        x='z'
+    if ctrl == 's':
+        print("backwards")
+        GPIO.output(motor1['in_1'], GPIO.LOW)
+        GPIO.output(motor1['in_2'], GPIO.LOW)
+        GPIO.output(motor2['in_1'], GPIO.LOW)
+        GPIO.output(motor2['in_2'], GPIO.HIGH)
+        GPIO.output(motor3['in_1'], GPIO.LOW)
+        GPIO.output(motor3['in_2'], GPIO.HIGH)
 
-    elif x=='l':
+    elif ctrl == 'l':
         print("low")
         p.ChangeDutyCycle(25)
-        x='z'
+        ctrl = 'z'
 
-    elif x=='m':
+    elif ctrl == 'm':
         print("medium")
         p.ChangeDutyCycle(50)
-        x='z'
+        ctrl = 'z'
 
-    elif x=='h':
+    elif ctrl == 'h':
         print("high")
         p.ChangeDutyCycle(75)
-        x='z'
+        ctrl = 'z'
 
-
-    elif x=='e':
+    elif ctrl == 'e':
         GPIO.cleanup()
         break
 
